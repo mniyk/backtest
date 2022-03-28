fd29e3d50ca5ca3615632f7d898cba9f2fbb4b9e
mniyk <my.name.is.yohei.kono@gmail.com>
2022-03-28 17:15:25 +0900

    Add: create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='backtest',
+    version='0.1.0',
+    author='mniyk',
+    author_email='my.name.is.yohei.kono@gmail.com',
+    description='backtest python library',
+    long_description=long_description,
+    url='https://github.com/mniyk/backtest.git',
+    packages=setuptools.find_packages(),
+    install_requires=['pandas'])
